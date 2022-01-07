e6fd31b8b3a12c420dc46019013ebe6578bb68bf
vithusan99 <65951485+vithusan99@users.noreply.github.com>
2022-01-07 15:59:42 +0530

    pearl commit

---
+++ src/components/features/nav-bar.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="nav-full">
+        <ul>
+            <li>Home</li>
+            <li>Attraction</li>
+            <li>Wildlife Streaming</li>
+            <li>Tourism News</li>
+            <li>About Us</li>
+            <li>Contact Us</li>
+        </ul>
+
+        dgfhdg
+        </div>
+</template>
+<script>
+export default {
+    name:'Navbar'
+}
+</script>
+<style scoped>
+
+.nav-full {
+    background-color: rgba(0, 0, 0, 0.637);
+    color: white;
+    position: fixed;
+    width: 100%;
+    
+ }
+ .nav-full ul{
+     padding: 0 !important;
+     margin: 0 !important;
+     text-align: center;
+ }
+.nav-full ul li{
+    position: sticky;
+    top: 0;
+    width: auto;
+    margin: 10px ;
+    display: inline-block;
+    
+    }
+    </style>
